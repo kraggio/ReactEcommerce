2aaa7c9ddc19ddb6ada80de47ca436ea520e4844
kraggio <keilyraggio18@gmail.com>
2023-04-05 12:18:13 -0400

    all images should load on this deployment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
     <footer>
       <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
         <img
-          src="Images/facebooklogo.png"
+          src="https://upload.wikimedia.org/wikipedia/commons/c/cd/Facebook_logo_%28square%29.png?20140103173026"
           alt="facebook logo"
           className="media"
         />
@@ -14,23 +14,31 @@ function Footer() {
 
       <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
         <img
-          src="Images/instagramlogo5.png"
+          src="https://upload.wikimedia.org/wikipedia/commons/5/58/Instagram-Icon.png"
           alt="tiktok logo"
           className="media"
         />
       </a>
 
-      <a href="https://www.tiktok.com/en/" target="_blank" rel="noreferrer">
-        <img src="Images/tiktok.png" alt="twitter logo" className="media" />
-      </a>
-
-      <a href="https://twitter.com/" target="_blank" rel="noreferrer">
+      <a
+        href="https://www.pinterest.com/search/pins/?q=studio%20ghibli%20art&rs=typed"
+        target="_blank"
+        rel="noreferrer"
+      >
         <img
-          src="Images/twitterlogo2.png"
-          alt="instagram logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/08/Pinterest-logo.png?20160129083321"
+          alt="twitter logo"
           className="media"
         />
       </a>
+
+      {/* <a href="https://twitter.com/" target="_blank" rel="noreferrer">
+        <img
+          src="https://image.similarpng.com/very-thumbnail/2020/06/Black-icon-Twitter-logo-transparent-PNG.png"
+          alt="instagram logo"
+          className="media"
+        />
+      </a> */}
     </footer>
   );
 }
